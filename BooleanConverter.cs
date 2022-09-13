25192323249ecf7c8530af7bd54eb2060845ffde
Can Karatepe <cankaratepe2000@gmail.com>
2022-09-13 15:13:48 +0300

    Initial commit

---
+++ BooleanConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FFmpegGui;
+
+public class BooleanConverter<T> : IValueConverter
+{
+    protected BooleanConverter(T trueValue, T falseValue)
+    {
+        True = trueValue;
+        False = falseValue;
+    }
+
+    public T True { get; set; }
+    public T False { get; set; }
+
+    public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (value is true ? True : False) ?? throw new InvalidOperationException();
+    }
+
+    public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is T castValue && EqualityComparer<T>.Default.Equals(castValue, True);
+    }
+}
+
+public sealed class CustomBooleanToVisibilityConverter : BooleanConverter<Visibility>
+{
+    public CustomBooleanToVisibilityConverter() : base(Visibility.Visible, Visibility.Collapsed)
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace FFmpegGui;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : INotifyPropertyChanged
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void ToggleUsingDuration()
+    {
+        IsUsingDuration = !IsUsingDuration;
+    }
+
+    private void LoadFile(string inputFilePath)
+    {
+        MessageBox.Show($"Loaded file: {inputFilePath}");
+    }
+
+    #region Properties
+
+    private bool _isCropEnabled;
+
+    public bool IsCropEnabled
+    {
+        get => _isCropEnabled;
+        set
+        {
+            if (value == _isCropEnabled)
+            {
+                return;
+            }
+
+            _isCropEnabled = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    private bool _isTrimEnabled;
+
+    public bool IsTrimEnabled
+    {
+        get => _isTrimEnabled;
+        set
+        {
+            if (value == _isTrimEnabled)
+            {
+                return;
+            }
+
+            _isTrimEnabled = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    private bool _isUsingDuration;
+
+    public bool IsUsingDuration
+    {
+        get => _isUsingDuration;
+        set
+        {
+            if (value == _isUsingDuration)
+            {
+                return;
+            }
+
+            _isUsingDuration = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    #endregion
+
+    #region Events
+
+    private void BtnConvert_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show(
+            $"IsCropEnabled: {IsCropEnabled}\nIsTrimEnabled: {IsTrimEnabled}\nIsUsingDuration: {IsUsingDuration}");
+    }
+
+    private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+    {
+        var openFileDialog = new OpenFileDialog();
+        if (!openFileDialog.ShowDialog().GetValueOrDefault())
+        {
+            return;
+        }
+
+        LoadFile(openFileDialog.FileName);
+        TxtFilePath.Text = openFileDialog.FileName;
+        TxtFilePath.Focus();
+        TxtFilePath.CaretIndex = TxtFilePath.Text.Length;
+        TxtFilePath.ScrollToEnd();
+    }
+
+    private void MenuDurationSwitch_Click(object sender, RoutedEventArgs e)
+    {
+        ToggleUsingDuration();
+    }
+
+    private void GrdToAndDurationGrid_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            ToggleUsingDuration();
+        }
+    }
+
+    #endregion
+}
