e1e35a1f1e46e809395c75028b88b1075427281f
Can Karatepe <cankaratepe2000@gmail.com>
2022-12-01 17:22:30 +0300

    Disable trim controls as they are not implemented

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
